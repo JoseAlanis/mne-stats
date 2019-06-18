174fec1383ad646ca6d9c76099776ffc6f1b4e88
Jose C. G. Alanis <12409129+JoseAlanis@users.noreply.github.com>
2019-06-18 23:14:03 +0200

    add dengemanns suggestions

--- examples/plot_linear_regression.py
+++ examples/plot_linear_regression.py
@@ -1,13 +1,20 @@
-# -- import necessary packages and modules
-from collections import namedtuple
+"""
+================================================================
+Plot beta coefficients from linear model estimation with sklearn
+================================================================
+
+"""
+
+# Authors: Jose C. Garcia Alanis <alanis.jcg@gmail.com>
+#
+# License: BSD (3-clause)
 
 import numpy as np
-from scipy import linalg, stats
 from sklearn.linear_model import LinearRegression
 
+from mne.decoding import Vectorizer, get_coef
 from mne.datasets import limo
 from mne.stats import linear_regression
-# from mne.source_estimate import SourceEstimate
 from mne.evoked import EvokedArray
 from mne.io.pick import pick_types
 
@@ -43,8 +50,8 @@ design = limo_epochs['2'].metadata.copy().assign(intercept=1)
 # effect code contrast for categorical variable (i.e., condition a vs. b)
 design['face a - face b'] = np.where(design['face'] == 'A', 1, -1)
 # create design matrix with named predictors
-predictors = ['intercept', 'face a - face b', 'phase-coherence']
-design = design[predictors]
+features = ['intercept', 'face a - face b', 'phase-coherence']
+design = design[features]
 
 ###############################################################################
 # --- run linear regression analysis using scikit-learn ---
@@ -52,84 +59,35 @@ design = design[predictors]
 # design matrix
 design_matrix = design.copy()
 # name of predictors
-names = predictors.copy()
+names = features.copy()
 
 # data to be analysed
 data = limo_epochs['2'].get_data(picks_eeg)
 # place holder for results
 out = EvokedArray(np.zeros(data.shape[1:]), epochs_info, tmin)
 
-# number of epochs in data set
-n_samples = len(data)
-# channels x time points
-n_features = np.product(data.shape[1:])
-
-# design matrix dimensions
-n_rows, n_predictors = design_matrix.shape
-
 # vectorised (channel) data for linear regression
-y = np.reshape(data, (n_samples, n_features))
+Y = Vectorizer().fit_transform(data)
 
 # fit linear model with sklearn
 # use fit intercept=false because b0 already in design matrix
-linaer_model = LinearRegression(fit_intercept=False).fit(design_matrix, y)
+linear_model = LinearRegression(fit_intercept=False)
+fit = linear_model.fit(design_matrix, Y)
 
-# extract coefficients (i.e., betas)
-betas = linaer_model.coef_.T
+# extract coefficients (i.e., betas) for estimator
+betas = get_coef(fit, 'coef_').T
 
-# save predictions and calculate residual sum of squares
-predictions = linaer_model.predict(design[predictors])
-resid_sum_squares = sum((y - predictions) ** 2)  # sum of squared residuals
+# store coefficients for each feature in a dictionary
+beta = dict()
+for coef, feature in zip(betas, features):
+    beta[feature] = coef.reshape(data.shape[1:])
 
-# degrees of freedom for inference
-df = n_rows - n_predictors
-
-# estimate and save linear regression results
-sqrt_noise_var = np.sqrt(resid_sum_squares / df).reshape(data.shape[1:])
-design_invcov = linalg.inv(np.dot(design_matrix.T, design_matrix))
-unscaled_stderrs = np.sqrt(np.diag(design_invcov))
-tiny = np.finfo(np.float64).tiny
-beta, stderr, t_val, p_val, mlog10_p_val = (dict() for _ in range(5))
-
-for x, unscaled_stderr, predictor in zip(betas, unscaled_stderrs, names):
-    beta[predictor] = x.reshape(data.shape[1:])
-    stderr[predictor] = sqrt_noise_var * unscaled_stderr
-    p_val[predictor] = np.empty_like(stderr[predictor])
-    t_val[predictor] = np.empty_like(stderr[predictor])
-
-    stderr_pos = (stderr[predictor] > 0)
-    beta_pos = (beta[predictor] > 0)
-    t_val[predictor][stderr_pos] = (beta[predictor][stderr_pos] /
-                                    stderr[predictor][stderr_pos])
-    cdf = stats.t.cdf(np.abs(t_val[predictor][stderr_pos]), df)
-    p_val[predictor][stderr_pos] = np.clip((1. - cdf) * 2., tiny, 1.)
-    # degenerate cases
-    mask = (~stderr_pos & beta_pos)
-    t_val[predictor][mask] = np.inf * np.sign(beta[predictor][mask])
-    p_val[predictor][mask] = tiny
-    # could do NaN here, but hopefully this is safe enough
-    mask = (~stderr_pos & ~beta_pos)
-    t_val[predictor][mask] = 0
-    p_val[predictor][mask] = 1.
-    mlog10_p_val[predictor] = -np.log10(p_val[predictor])
-
-# linear regression results
-lm_params = beta, stderr, t_val, p_val, mlog10_p_val
-
-# create epochs objects for storage of results
-lm = namedtuple('lm', 'beta stderr t_val p_val mlog10_p_val')
-lm_fits = {}
-
-# loop through predictors and save results
-for name in names:
-    parameters = [p[name] for p in lm_params]
-    for ii, value in enumerate(parameters):
-        out_ = out.copy()
-        # if not isinstance(out_, (SourceEstimate, Evoked)):
-        #     raise RuntimeError('Invalid container.')
-        out_._data[:] = value
-        parameters[ii] = out_
-    lm_fits[name] = lm(*parameters)
+# loop through predictors and save results in evoked object
+lm_betas = {}
+for feature in features:
+    out_ = out.copy()
+    out_.data[:] = beta[feature]
+    lm_betas[feature] = out_
 
 ###############################################################################
 # --- plot results of linear regression ---
@@ -137,15 +95,15 @@ for name in names:
 ts_args = dict(xlim=(-.25, 0.5))
 
 # visualise effect of phase-coherence for sklearn estimation method.
-lm_fits['phase-coherence'].beta.plot_joint(ts_args=ts_args,
-                                           title='Phase-coherence (sklearn)',
-                                           times=[.23])
+lm_betas['phase-coherence'].plot_joint(ts_args=ts_args,
+                                       title='Phase-coherence (sklearn betas)',
+                                       times=[.23])
 
 ###############################################################################
-# replicate analysis with using mne.stats.linear_regression
-reg = linear_regression(limo_epochs['2'], design, names=predictors)
+# replicate analysis using mne.stats.linear_regression
+reg = linear_regression(limo_epochs['2'], design, names=features)
 
 # visualise effect of phase-coherence for mne.stats method.
 reg['phase-coherence'].beta.plot_joint(ts_args=ts_args,
-                                       title='Phase-coherence (mne.stats)',
+                                       title='Phase-coherence (mne.stats betas)',  # noqa
                                        times=[.23])
