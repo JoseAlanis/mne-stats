ee4293981269ec6509ca1367f04ac5737243be0f
Jose C. G. Alanis <12409129+JoseAlanis@users.noreply.github.com>
2019-07-08 22:23:44 +0200

    add dengemans suggestion from #6  / simplify code

--- examples/plot_bootstrap_ci.py
+++ examples/plot_bootstrap_ci.py
@@ -12,12 +12,9 @@ Plot bootstrapped confidence interval for condition ERP
 import numpy as np
 import matplotlib.pyplot as plt
 
-from mne import create_info
 from mne.viz import plot_compare_evokeds
-from mne.channels import read_montage
 from mne.decoding import Vectorizer
 from mne.datasets import limo
-from mne.io.pick import pick_types
 
 ###############################################################################
 # list with subjects ids that should be imported
@@ -32,44 +29,9 @@ limo_epochs = {str(subj): limo.load_data(subject=subj) for subj in subjects}
 for subject in limo_epochs.values():
     subject.interpolate_bads(reset_bads=True)
 
-# montage for info
-montage = read_montage('biosemi128')
-
-# pick channels that should be included in the analysis
-picks_eeg = pick_types(limo_epochs['2'].info, eeg=True)
-# channel names and types
-ch_names = limo_epochs['2'].info['ch_names'][0:len(picks_eeg)]
-ch_types = ['eeg'] * len(ch_names)
-# channels to be excluded
-exclude = ['EXG1', 'EXG2', 'EXG3', 'EXG4']
-# sampling rate
-sfreq = limo_epochs['2'].info['sfreq']
-
-# create evoked info for results object
-evoked_info = create_info(ch_names=ch_names, sfreq=sfreq, ch_types=ch_types,
-                          montage=montage)
-
-# save epochs information (needed for creating a homologous
-# epochs object containing linear regression result)
-epochs_info = limo_epochs['2'].copy().drop_channels(exclude).info
-tmin = limo_epochs['2'].tmin
-
 ###############################################################################
-# use epochs metadata to create design matrix for linear regression analyses
-
-# add intercept
-design = limo_epochs['2'].metadata.copy().assign(intercept=1)
-# effect code contrast for categorical variable (i.e., condition a vs. b)
-design['face a - face b'] = np.where(design['face'] == 'A', 1, -1)
-# create design matrix with named predictors
-predictors = ['intercept', 'face a - face b', 'phase-coherence']
-design = design[predictors]
-
-###############################################################################
-# --- run linear regression analysis using scikit-learn ---
 # data to be analysed
-face_a = limo_epochs['2']['Face/A'].get_data(picks_eeg)
-# face_b = limo_epochs['2']['Face/B'].get_data(picks_eeg)
+face_a = limo_epochs['2']['Face/A'].get_data()
 
 # number of trials per condition
 n_epochs = face_a.shape[0]
@@ -78,24 +40,32 @@ n_epochs = face_a.shape[0]
 # we'll this this information later to bring the results of the
 # the linear regression algorithm into an eeg-like format
 # (i.e., channels x times points)
-n_channels = len(picks_eeg)
+n_channels = len(limo_epochs['2'].info['ch_names'])
 n_times = len(limo_epochs['2'].times)
 
 # vectorize (channel) data for linear regression
 Y = Vectorizer().fit_transform(face_a)
 
+###############################################################################
+# run bootstrap for centrality estimator
+
 # set random state for replication
 random_state = 42
-np.random.seed(random_state)
+random = np.random.RandomState(random_state)
 
 # number of random samples
 boot = 2000
 
-# bootstrap estimator
+# initialize bootstrap
 resampled_data = []
 for i in range(boot):
-    resamples = np.random.choice(range(n_epochs), n_epochs)
+    # pick random samples with replacement from data
+    resamples = random.choice(range(n_epochs), n_epochs, replace=True)
+    # compute centrality estimator on re-sampled data
     resampled_data.append(Y[resamples, :].mean(axis=0))
+
+###############################################################################
+# compute confidence interval
 
 # compute low and high percentile
 lower, upper = np.quantile(resampled_data, [.025, .975], axis=0)
@@ -106,15 +76,21 @@ lower = lower.reshape((n_channels, n_times))
 # upper bound
 upper = upper.reshape((n_channels, n_times))
 
+###############################################################################
+# plot results
+
 # erp for condition
 face_a_erp = limo_epochs['2']["Face/A"].average()
 
 # electrode to plot
-pick = face_a_erp.ch_names.index('B29')
+pick = face_a_erp.ch_names.index('B8')
 
+# create figure
 fig, ax = plt.subplots(figsize=(10, 7))
-ax = plot_compare_evokeds(face_a_erp, pick, ylim=dict(eeg=[-3, 5]),
-                          colors=['b'], axes=ax)
+ax = plot_compare_evokeds(face_a_erp, pick,
+                          ylim=dict(eeg=[-17.5, 10]),
+                          show_sensors='upper right',
+                          axes=ax)
 ax.axes[0].fill_between(limo_epochs['2']["Face/A"].times,
                         upper[pick]*1e6,
                         lower[pick]*1e6, alpha=0.25)
